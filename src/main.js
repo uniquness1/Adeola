579978941d8c0dc052607fc0b2cec59af35b77bf
Ajijola Adeola <aajijola3@gmail.com>
2025-09-25 15:42:55 +0100

    analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import emailjs from "@emailjs/browser";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 emailjs.init("nl8cnmpaBLgCdQsHh");
 const app = createApp(App);
